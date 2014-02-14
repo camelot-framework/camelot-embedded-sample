8656dcadabc819846cc6e96d01e50fbb0b0a4b58
Ilya Sadykov <smecsia@gmail.com>
2014-02-14 19:33:37 +0400

    PluginsContextIntoBeansInjector

--- src/main/java/ru/yandex/qatools/terra/example/IndexResource.java
+++ src/main/java/ru/yandex/qatools/terra/example/IndexResource.java
@@ -7,16 +7,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import static ru.yandex.qatools.clay.utils.DateUtil.timestamp;
-
 /**
  * @author: Ilya Sadykov (mailto: smecsia@yandex-team.ru)
  */
 @Path("/")
 @Component
 public class IndexResource {
-    private static final long startedAt = timestamp();
-
     @GET
     @Produces({MediaType.APPLICATION_JSON + ";charset=utf-8"})
     public String index() {
